cbafdf63c19c467acdae9c5067b6f49905f724aa
mskDev0092 <102049709+mskDev0092@users.noreply.github.com>
2023-08-25 20:25:13 +0500

    2 files modified

--- src/Components/Drum.jsx
+++ src/Components/Drum.jsx
@@ -5,46 +5,55 @@ const DATA = [
   {
     id: 'Heater 1',
     key: 'Q',
+    keycode: 81,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
   },
   {
     id: 'Heater 2',
     key: 'W',
+    keycode: 87,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
   },
   {
     id: ' Heater 3',
     key: 'E',
+    keycode: 69,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
   },
   {
     id: 'Heater 4 ',
     key: 'A',
+    keycode: 65,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
   },
   {
     id: 'Clap',
     key: 'S',
+    keycode: 83,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
   },
   {
     id: 'Open-HH',
     key: 'D',
+    keycode: 68,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
   },
   {
     id: "Kick-n'-Hat",
     key: 'Z',
+    keycode: 90,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
   },
   {
     id: 'Kick',
     key: 'X',
+    keycode: 88,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
   },
   {
     id: 'Closed-HH',
     key: 'C',
+    keycode: 67,
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
   },
 ];
@@ -53,7 +62,7 @@ export default function Drum() {
   const [display, setDisplay] = useState('Welcome');
   const [url, setUrl] = useState();
   const [volume, setVolume] = useState(34);
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(true);
   const toggle = ['display', 'drum-pad'];
 
   const audio = new Audio();
@@ -79,16 +88,19 @@ export default function Drum() {
       <div className="btn">
         <button
           onClick={() => setIsActive(!isActive)}
-          className={isActive ? 'active' : null}
+          className={isActive ? 'activePads' : 'offPads'}
         ></button>
       </div>
       <div>
         <input
           type="range"
+          min="1"
+          max="100"
           value={volume}
           onChange={(e) => setVolume(e.target.value)}
         />
       </div>
+
       <div className="drum-pad" id="drum-pad">
         {DATA.map((e) => (
           <button key={e.id} onClick={() => handleClick(e)}>
